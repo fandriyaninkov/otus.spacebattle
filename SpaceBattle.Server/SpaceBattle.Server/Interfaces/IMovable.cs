d97402526565f7bbfb7e1b73ca55247cd6eb761d
Fedor Andriyaninkov <fandriyaninkov@gmail.com>
2022-04-26 08:44:25 +0300

    реализация классов перемещения и вращения

---
+++ SpaceBattle.Server/SpaceBattle.Server/Interfaces/IMovable.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+namespace SpaceBattle.Server.Interfaces;
+
+public interface IMovable
+{
+    /// <summary>
+    /// Получить текущую позицию
+    /// </summary>
+    /// <returns></returns>
+    Vector2? GetPosition();
+
+    /// <summary>
+    /// Получить текущее смещение
+    /// </summary>
+    /// <returns></returns>
+    Vector2? GetVelocity();
+
+    /// <summary>
+    /// Установить новую позицию
+    /// </summary>
+    /// <param name="newValue"></param>
+    void SetPosition(Vector2 newValue);
+}
---
+++ SpaceBattle.Server/SpaceBattle.Server/Movable.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+using SpaceBattle.Server.Interfaces;
+
+namespace SpaceBattle.Server;
+
+public class Movable : IMovable
+{
+    private Vector2? _currentPosition;
+    private Vector2? _curentVelocity;
+
+    public Vector2? GetPosition()
+    {
+        return _currentPosition;
+    }
+
+    public Vector2? GetVelocity()
+    {
+        return _curentVelocity;
+    }
+
+    public void SetPosition(Vector2 newValue)
+    {
+        if (!float.IsFinite(newValue.X) || !float.IsFinite(newValue.Y))
+        {
+            throw new ArgumentException("Новая позиция не валидна");
+        }
+        _currentPosition = newValue;
+    }
+}
